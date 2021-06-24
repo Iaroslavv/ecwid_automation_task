5ea213047f82943f40f6195e6bb574c66d6cb1cd
Iaroslav Bulimov <yaroslaw.bulimov@yandex.ru>
2021-06-24 15:25:54 +0300

    fix assertion

--- pages/search_page.py
+++ pages/search_page.py
@@ -8,8 +8,7 @@ class SearchPage(BasePage):
     
     def check_search_results_contain_keyword(self, search_item):
         assert self.find_items_from_search_results(*SearchPageLocators.SEARCH_ITEMS, search_item), "Not all search results " \
-                                                                                                   "with the keyword are shown"
-
+                                                                                                   "include the keyword"
 
     def check_search_results(self):
         assert self.is_element_present(*SearchPageLocators.SEARCH_RESULTS), "There aren't any search results"
