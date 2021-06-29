24cbaf8b5fcab99a0d6944e4898274e25532f010
Iaroslav Bulimov <yaroslaw.bulimov@yandex.ru>
2021-06-29 17:39:16 +0300

    change sort dropdown and sort asc

--- pages/locators.py
+++ pages/locators.py
@@ -10,5 +10,5 @@ class SearchPageLocators:
     SEARCH_RESULTS = (By.CSS_SELECTOR, "div.grid__products")
     SEARCH_ITEMS = (By.CSS_SELECTOR, "div.grid__products>div.grid-product >div.grid-product__wrap>div.grid-product__wrap-inner>a")
     ITEM_PRICE = (By.CSS_SELECTOR, ".grid-product__price-amount >div")
-    SORT_DROPDOWN_BUTTONS = (By.XPATH, "//div[@class='ec-filter ec-filter--sortby ec-filter--section-sticky-bar']/div/div/div")
-    SORT_ASC_BUTTON = (By.CSS_SELECTOR,"[for='radio-sortBy-priceAsc']")
+    SORT_DROPDOWN = (By.CSS_SELECTOR, "#ec-products-sort")
+    SORT_ASC_BUTTON = (By.CSS_SELECTOR,"[value='priceAsc']")
