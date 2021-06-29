7305007b26aae71494bfe2fda0539a35bbd8641e
Iaroslav Bulimov <yaroslaw.bulimov@yandex.ru>
2021-06-29 23:35:26 +0300

    fix string

--- test_search_page.py
+++ test_search_page.py
@@ -2,7 +2,7 @@ from pages.search_page import SearchPage
 from pages.variables import search_parameters
 import pytest
 
-@pytest.mark.parametrize('domen', ["ru"])
+@pytest.mark.parametrize('domain', ["ru"])
 @pytest.mark.search_field
 class TestSearchField:
     """
@@ -10,8 +10,8 @@ class TestSearchField:
     string options.
     """
     @pytest.mark.xfail(reason="not all results're relevant to the keyword")
-    def test_correct_input_search_field(self, browser, domen):
-        link = f"https://www.ecwid.{domen}/demo/search"
+    def test_correct_input_search_field(self, browser, domain):
+        link = f"https://www.ecwid.{domain}/demo/search"
         page = SearchPage(browser, link)
         page.open()
         search_item = "dress"
@@ -21,16 +21,16 @@ class TestSearchField:
         page.check_search_results_contain_keyword(search_item)
 
     @pytest.mark.xfail(reason="it's impossible to click on a search icon")
-    def test_empty_input(self, browser, domen):
-        link = f"https://www.ecwid.{domen}/demo/search"
+    def test_empty_input(self, browser, domain):
+        link = f"https://www.ecwid.{domain}/demo/search"
         page = SearchPage(browser, link)
         page.open()
         page.click_on_search_field_button()
         page.check_search_results()
 
     @pytest.mark.parametrize("word", search_parameters)
-    def test_search_field_inputs(self, browser, domen, word):
-        link = f"https://www.ecwid.{domen}/demo/search"
+    def test_search_field_inputs(self, browser, domain, word):
+        link = f"https://www.ecwid.{domain}/demo/search"
         page = SearchPage(browser, link)
         page.open()
         search_item = f"{word}"
