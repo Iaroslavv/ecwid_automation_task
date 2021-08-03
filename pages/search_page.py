4017f1c648e658e70bca7cf6388263ff5902e838
Iaroslav Bulimov <yaroslaw.bulimov@yandex.ru>
2021-08-03 19:54:33 +0300

    dots

--- pages/search_page.py
+++ pages/search_page.py
@@ -3,7 +3,7 @@ from pages.locators import SearchPageLocators
 
 class SearchPage(BasePage):
     """
-    Contains assert methods checking if functionality is working properly.
+    Contains assert methods checking if functionality is working properly...
     """
     
     def check_search_results_contain_keyword(self, search_item):
