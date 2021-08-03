45851afdac8f315ace89fe9de14871e43cb9532b
Iaroslav Bulimov <yaroslaw.bulimov@yandex.ru>
2021-08-03 20:02:36 +0300

    ddd

--- pages/search_page.py
+++ pages/search_page.py
@@ -3,7 +3,7 @@ from pages.locators import SearchPageLocators
 
 class SearchPage(BasePage):
     """
-    Contains assert methods checking if functionality is working properseaasd sdsd
+    Contains assert methods checking if functionality 
     """
     
     def check_search_results_contain_keyword(self, search_item):
